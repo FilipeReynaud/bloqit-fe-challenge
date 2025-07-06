d6f41f336e20fdc0cbc6286af33cdd10603014d8
Filipe Reynaud <filipe.reynaud@riskified.com>
2025-07-06 17:25:39 +0100

    feat(pokedex): boilerplate

---
+++ pokedex/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@bloqit-fe-challenge/pokedex" />
+    </div>
+  );
+}
+
+export default App;
